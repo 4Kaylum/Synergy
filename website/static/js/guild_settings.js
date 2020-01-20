6bcdc66dca665d9829a39ab25f5db704da2ac6c3
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-01-20 13:23:31 +0000

    Add submit changes button

--- website/static/js/guild_settings.js
+++ website/static/js/guild_settings.js
@@ -1,7 +1,7 @@
 function addNewResponse(formNode) {
     const newResponseTag = $(`
         <div class="input-group mt-1">
-            <input type="text" class="form-control" placeholder="A response for an interaction" value="" />
+            <input type="text" class="form-control" placeholder="A response for an interaction" value="" required />
             <div class="input-group-append">
                 <button class="btn btn-danger" type="button" onclick="$(this.parentNode.parentNode).remove()">-</button>
             </div>
@@ -15,6 +15,7 @@ function addNewResponse(formNode) {
 function addNewCommand() {
     var commandName = prompt("What's the name of the command you want to add?", "").trim();
     if(commandName == "") return;
+    if(!commandName.match(/^[\d\w ]+$/)) return alert("That command name is invalid.");
     const newCommandTag = $(`
         <div class="accordion w-100" id="guild-settings-accordion">
             <form class="card interaction-form" data-interaction-name="${commandName}">
@@ -37,8 +38,8 @@ function addNewCommand() {
             </form>
         </div>
     `);
-    const accordionHolder = $('#guild-settings-accordion-holder')
-    accordionHolder.append(newCommandTag);
+    const accordionHolder = $('#guild-settings-accordion-holder');
+    $(accordionHolder.children()[accordionHolder.children().length - 1]).before(newCommandTag);
 }
 
 
@@ -47,7 +48,17 @@ function getInteractionResponses() {
     var output = {};
     Array.from(formList).forEach(element => {
         var jq = $(element);
-        output[element.dataset['interactionName']] = Array.from(jq.find('input')).map(x => x.value);
+        output[element.dataset['interactionName']] = Array.from(jq.find('input'))
+            .map(x => x.value.trim())
+            .filter(x => x.length > 0);
     });
     return output;
+}
+
+
+function submitCommandChanges() {
+    const commandList = getInteractionResponses();
+    for (let [commandName, responseList] of Object.entries(commandList)) {
+        console.log(`${commandName}: ${responseList}`);
+    }
 }
