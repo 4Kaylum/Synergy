a5a3aab0a67c27b08644263755d01b2f256c0420
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-01-22 08:03:01 +0000

    Add copy to guild page

--- website/frontend.py
+++ website/frontend.py
@@ -1,9 +1,10 @@
 import collections
 
-from aiohttp.web import RouteTableDef, Request, HTTPFound
-from aiohttp_jinja2 import template
+from aiohttp.web import RouteTableDef, Request, HTTPFound, json_response
+from aiohttp_jinja2 import template, render_template
 import aiohttp_session
 import discord
+import asyncpg
 
 from website import utils as webutils
 
@@ -48,7 +49,7 @@ async def guild_picker(request:Request):
 
 
 @routes.get("/guilds/{guild_id}")
-@template('guild_settings.j2')
+# @template('guild_settings.j2')
 @webutils.add_output_args()
 async def guild_settings(request:Request):
     """The guild picker page for the user"""
@@ -88,10 +89,88 @@ async def guild_settings(request:Request):
         interactions[response['command_name']].append(response['response'])
 
     # Send data back to page
+    return render_template(
+        'guild_settings.dev.j2' if request.query.get('dev') else 'guild_settings.j2',
+        request,
+        {
+            'guild': guild_object,
+            'interactions': interactions,
+            'session': session
+        },
+    )
+
+
+@routes.get("/copy_to_guild")
+@template('copy_to_guild.j2')
+@webutils.add_output_args()
+async def copy_to_guild(request:Request):
+    """Copies data to a guild of yours"""
+
+    # Check session age
+    session = await aiohttp_session.get_session(request)
+    if session.new:
+        return HTTPFound(location='/')
+
+    # Build data
+    query_data = collections.defaultdict(list)
+    for i, o in request.query.items():
+        query_data[i].append(o)
+    output_data = []
+    for i, o in zip(query_data['command'], query_data['response']):
+        output_data.append((i, o))
+
+    # Return information
+    user_guilds = await webutils.get_user_guilds(request)
     return {
-        'guild': guild_object,
-        'interactions': interactions
+        'user_guilds': [i for i in user_guilds if discord.Permissions(i['permissions']).manage_messages],
+        'query_data': output_data,
     }
+
+
+@routes.get("/copy_to_guild/{guild_id}")
+async def copy_to_guild_redirect(request:Request):
+    """Copies data to the guild and then redirects to its settings page"""
+
+    # Validate guild ID
+    try:
+        guild_id = int(request.match_info['guild_id'])
+    except ValueError:
+        return HTTPFound(location='/guilds')
+
+    # Check session age
+    session = await aiohttp_session.get_session(request)
+    if session.new:
+        return HTTPFound(location='/')
+
+    # Check user permissions
+    if session['user_id'] not in request.app['config']['owners']:
+        user_guilds = await webutils.get_user_guilds(request)
+        guild_info = [i for i in user_guilds if guild_id == i['id'] and discord.Permissions(i['permissions']).manage_messages],
+        if not guild_info:
+            return HTTPFound(location='/guilds')
+
+    # Build data
+    query_data = collections.defaultdict(list)
+    for i, o in request.query.items():
+        query_data[i].append(o)
+    output_data = []
+    for i, o in zip(query_data['command'], query_data['response']):
+        output_data.append((i, o))
+
+    # Save data
+    async with request.app['database']() as db:
+        for command, response in output_data:
+            try:
+                await db('INSERT INTO command_names (guild_id, command_name) VALUES ($1, $2)', guild_id, command)
+            except asyncpg.UniqueViolationError:
+                pass
+            try:
+                await db('INSERT INTO command_responses (guild_id, command_name, response) VALUES ($1, $2, $3)', guild_id, command, response)
+            except asyncpg.UniqueViolationError:
+                pass
+
+    # Redirect
+    return HTTPFound(location=f"/guilds/{guild_id}")
 
 
 @routes.get("/discord_oauth_login")
--- website/static/js/marketplace.js
+++ website/static/js/marketplace.js
@@ -10,6 +10,13 @@ function populateCommandDeck() {
                     <div class="card-body">
                         ${element.response.replace('%author', '<code>%author</code>').replace('%user', '<code>%user</code>')}
                     </div>
+                    <div class="card-footer">
+                        <form action="/copy_to_guild">
+                            <input type="hidden" name="command" value="${element.command_name}" />
+                            <input type="hidden" name="response" value="${element.response}" />
+                            <button class="btn w-100" formmethod="get" type="submit">Copy to Guild</button>
+                        </form>
+                    </div>
                 </div>
             `))
         });
