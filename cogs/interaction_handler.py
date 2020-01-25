756a37bfaf05c6b1165c902b2a6b5ec40cfa682d
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-01-25 11:46:14 +0000

    Better handle command invoking and argument parsing

--- cogs/interaction_handler.py
+++ cogs/interaction_handler.py
@@ -1,11 +1,14 @@
 import re as regex
 
+import discord
 from discord.ext import commands
 
 from cogs import utils
 
 
 class InteractionHandler(utils.Cog):
+
+    ARGUMENT_REPLACEMENT_REGEX = regex.compile(r"%(author|user(\d+)?)")
 
     @utils.Cog.listener("on_command_error")
     async def custom_command_listener(self, ctx:utils.Context, error:commands.CommandError):
@@ -21,30 +24,44 @@ class InteractionHandler(utils.Cog):
             responses = await db("SELECT command_responses.response FROM command_responses, command_names WHERE command_responses.guild_id=command_names.guild_id AND command_responses.command_name=$1 and command_responses.guild_id=$2 ORDER BY RANDOM()", command_name, ctx.guild.id)
         if not responses:
             return
+
+        # Invoke command
         metacommand: utils.Command = self.bot.get_command('interaction_response_metacommand')
         ctx.command = metacommand
         ctx.responses = responses
-        await self.bot.invoke(ctx)
+        try:
+            await ctx.command.invoke(ctx)  # This converts the args for me, which is nice
+        except commands.CommandError as e:
+            self.bot.dispatch("command_error", ctx, e)
 
     @commands.command(cls=utils.Command, hidden=True)
     @commands.guild_only()
-    async def interaction_response_metacommand(self, ctx:utils.Context):
+    async def interaction_response_metacommand(self, ctx:utils.Context, users:commands.Greedy[discord.Member]):
         """Handles pinging out the responses for a given interaction. Users cannot call this."""
 
         # Make sure that my users aren't bein fuckin dumb
         if ctx.invoked_with.lower() == 'interaction_response_metacommand':
             return  # :/
 
-        # Find the user mentioned in the message
-        matches = regex.search(f"(?:{ctx.invoked_with} )(.*)", ctx.message.content)
-        if not matches:
-            raise utils.errors.MissingRequiredArgumentString('user')
-        user = await commands.MemberConverter().convert(ctx, matches.group(1).split(' ')[0])
+        # Get user amount
+        MAX_MENTIONS = 1  # TODO read from db
+        MIN_MENTIONS = 1  # TODO read from db
+        if len(users) > MAX_MENTIONS:
+            return await ctx.send("You've mentioned too many users for this command.")  # TODO raise custom error
+        if len(users) < MIN_MENTIONS:
+            return await ctx.send("You've not mentioned enough users for this command.")  # TODO raise custom error
+
+        # Build command response
+        def argument_replacer(match):
+            """Replaces the argument of the group with a user mention"""
+
+            if match.group(2):
+                return users[int(match.group(2))].mention
+            return ctx.author.mention
 
         # Output
         text = ctx.responses[0]['response']
-        await ctx.send(text.replace(r"%author", ctx.author.mention).replace(r"%user", user.mention))
-
+        await ctx.send(self.ARGUMENT_REPLACEMENT_REGEX.sub(argument_replacer, text))
 
     @commands.command(cls=utils.Command)
     @commands.guild_only()
